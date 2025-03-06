2681b25d51792994b826ed9ae75255e313092007
イヨンウ <lee.yong.woo@arsaga.jp>
2025-03-07 00:44:12 +0900

    fix

--- Formula/mpm.rb
+++ Formula/mpm.rb
@@ -9,10 +9,10 @@ class Mpm < Formula
     depends_on "go" => :build
 
     if Hardware::CPU.arm?
-        url "https://github.com/ywl0806/mpm/releases/download/v1.0.0/mpm_darwin_arm64.tar.gz"
+        url "https://github.com/ywl0806/mpm/releases/download/v1.0.0/mpm-darwin-arm64.tar.gz"
         sha256 "26c884ab5eb0fcb0d4394ac387d98974b2066fe40b524f9eb62eea6d864a6901"
       else
-        url "https://github.com/ywl0806/mpm/releases/download/v1.0.0/mpm_darwin_amd64.tar.gz"
+        url "https://github.com/ywl0806/mpm/releases/download/v1.0.0/mpm-darwin-amd64.tar.gz"
         sha256 "b051d88c853ec12cb367be150efae3634cdc234f4316b9fab5090a318c8b53b6"
       end
 
